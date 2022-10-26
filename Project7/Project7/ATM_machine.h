bface022cb72e2b36ad6d264c111ff657920687c
Shadow5x5 <69230424+Shadow5x5@users.noreply.github.com>
2022-10-27 01:50:56 +0300

    Add files via upload

--- Project7/Project7/ATM_machine.h
+++ Project7/Project7/ATM_machine.h
@@ -5,9 +5,16 @@
 #include "Payment_for_the_phone.h"
 #include "Payment_of_a_fine_from_the_traffic_police.h"
 #include "Payment_TV.h"
+#include <list>
 class ATM_machine
 {
 private:
+	struct Banknote
+	{
+		int nominal_value;
+		int quantity;
+	};
+	std::list<Banknote> Banknotes;
 	Payment_for_the_phone Transaction1;
 	Payment_of_a_fine_from_the_traffic_police Transaction2;
 	Payment_TV Transaction3;
@@ -22,42 +29,83 @@ private:
 		std::cout << BC.getBalance() << std::endl;
 		system("pause");
 	}
+	int money_at_the_ATM() {
+		int sum = 0;
+		for (Banknote a : Banknotes) {
+			sum += (a.quantity * a.nominal_value);
+		}
+		return sum;
+	}
 	void cash_withdrawals(Bank_card& BC) {
-		int number;
-		std::cout << "1:  25" << std::endl;
-		std::cout << "2:  50" << std::endl;
-		std::cout << "3: 100" << std::endl;
-		std::cin >> number;
-		switch (number)
-		{
-		case 1: 
-			if (BC.getBalance() < 25) {
-				std::cout << "There are not enough funds in your account!!!" << std::endl;
+		int sum;
+		std::cout << "How much money do you want to cash out from the card?" << std::endl;
+		std::cin >> sum;
+		for (Banknote a : Banknotes) {
+			std::cout << a.quantity << std::endl;
+		}
+		if (money_at_the_ATM() < sum) {
+			std::cout << money_at_the_ATM() << std::endl;
+			return;
+		}
+		else {
+			int array[4];
+			int sum2 = 0;
+			int i = 0;
+			for (Banknote& a : Banknotes) {
+				if (a.quantity == 0) {
+					break;
+				}
+				switch (i++)
+				{
+				case 3: array[0] = (sum - array[1] * 25 - (array[2] * 50) - (array[3] * 100)) / 1; break;
+				case 2: array[1] = (sum - array[2] * 50 - (array[3] * 100)) / 25; break;
+				case 1: array[2] = (sum - array[3] * 100) / 50; break;
+				case 0: array[3] = sum / 100; break;
+				default:
+					break;
+				}
+			}
+			i = 0;
+			for (Banknote& a : Banknotes) {
+				if (a.quantity < array[i]) {
+					continue;
+				}
+				else {
+					a.quantity -= array[i];
+					sum2 += a.nominal_value * array[i];
+				}
+				i++;
+			}
+			if (sum == sum2) {
+				std::cout << "The money has been successfully cashed out" << std::endl;
 			}
 			else {
-				BC.setBalance(BC.getBalance() - 25); 
-			}break;
-		case 2:
-			if (BC.getBalance() < 50) {
-				std::cout << "There are not enough funds in your account!!!" << std::endl;
+				std::cout << "Insufficient funds at the ATM" << std::endl;
 			}
-			else {
-				BC.setBalance(BC.getBalance() - 50);
-			}break;
-		case 3: 
-			if (BC.getBalance() < 100) {
-				std::cout << "There are not enough funds in your account!!!" << std::endl;
-			}
-			else {
-				BC.setBalance(BC.getBalance() - 100);
-			}break;
-		default:
-			break;
+
 		}
 		system("pause");
 	}
 public:
-	
+	ATM_machine() {
+		Banknote a;
+		a.nominal_value = 1;
+		a.quantity = rand() % 25 + 1;
+		Sleep(1000);
+		Banknotes.push_back(a);
+		a.nominal_value = 25;
+		a.quantity = rand() % 10 + 1;
+		Sleep(1000);
+		Banknotes.push_back(a);
+		a.nominal_value = 50;
+		a.quantity = rand() % 10 + 1;
+		Sleep(1000);
+		Banknotes.push_back(a);
+		a.nominal_value = 100;
+		a.quantity = rand() % 10 + 1;
+		Sleep(1000);
+		Banknotes.push_back(a);
+	}
 	void Inserting_a_bank_card(Bank_card& BC) {
 		int number, pincode, attempts = 3;
 		while (attempts and BC.getCardBlocking()) {
--- Project7/Project7/Payment_TV.h
+++ Project7/Project7/Payment_TV.h
@@ -6,34 +6,43 @@ private:
 	std::list<int> Personal_account_database;
 public:
 	Payment_TV() {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 3; i++) {
 			srand(time(NULL));
 			Personal_account_database.push_back(rand() % 100 + 1);
 			Sleep(1000);
 		}
 	}
 	void Payment(Bank_card& BC) override {
+		bool flag = false;
 		for (int a : Personal_account_database) {
 			std::cout << a << " ";
 		}
 		std::cout << std::endl;
 		std::string PersonalAccountNumber;
+		double Money;
+		std::cout << "Personal account number: ";
+		std::cin >> PersonalAccountNumber;
 		for (int a : Personal_account_database) {
 			if (std::to_string(a) == PersonalAccountNumber) {
-				std::cout << "There is no such account!!!" << std::endl;
+				flag = true;
+				break;
+			}
+		}
+		if (flag) {
+			std::cout << "Amount of money: ";
+			std::cin >> Money;
+			if (Money > BC.getBalance()) {
+				std::cout << "There are not enough funds in your account!!!" << std::endl;
+				system("pause");
 				return;
 			}
-			else {
-				double Money;
-				std::cout << "Personal account number: ";
-				std::cin >> PersonalAccountNumber;
-				std::cout << "Amount of money: ";
-				std::cin >> Money;
-				if (Money > BC.getBalance()) {
-					std::cout << "There are not enough funds in your account!!!" << std::endl;
-				}
-				BC.setBalance(BC.getBalance() - Money);
-			}
+			BC.setBalance(BC.getBalance() - Money);
+
+		}
+		else {
+			std::cout << "There is no such account!!!" << std::endl;
+			system("pause");
+			return;
 		}
 	}
 };
